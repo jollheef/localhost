265a716ce579b12ddee44f0afbb7a8b0e0250597
Mikhail Klementev <blame@dumpstack.io>
2019-07-18 20:52:21 +0000

    Manage zsh with home-manager

--- home-manager.nix
+++ home-manager.nix
@@ -20,11 +20,51 @@ in {
           key = "0x1525585D1B43C62A";
         };
       };
+
+      zsh = {
+        enable = true;
+
+        oh-my-zsh = {
+          enable = true;
+          theme = "gentoo";
+          plugins = [ "git" "cp" "tmux-my" ];
+          custom = "$HOME/.oh-my-zsh-custom";
+        };
+
+        sessionVariables = {
+          LC_ALL = "en_US.utf8";
+          LIBVIRT_DEFAULT_URI = "qemu:///system";
+          GOPATH = "\${HOME}";
+          PATH = "\${PATH}:\${HOME}/bin";
+
+          GPG_TTY = "$(tty)";
+          SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
+
+          ZSH_TMUX_AUTOSTART = "true";
+          ZSH_TMUX_AUTOCONNECT = "true";
+        };
+
+        shellAliases = {
+          mutt = "neomutt";
+          vim = "emacs -nw";
+          emacs = "emacs -nw";
+          clipimage = "xclip -selection clipboard -target image/png -out > out.png";
+        };
+
+        initExtra = ''
+          gpgconf --launch gpg-agent
+          which apt >/dev/null 2>&1 && plugins=("\$\{(@)plugins:#tmux-my\}")
+        '';
+      };
     };
 
     home.file = {
       ".emacs.d/init.el".source = ./etc/emacs.el;
       ".xmonad/xmonad.hs".source = ./etc/xmonad.hs;
+
+      ".oh-my-zsh-custom/plugins/tmux-my/tmux-my.extra.conf".source = ./etc/tmux-my/tmux-my.extra.conf;
+      ".oh-my-zsh-custom/plugins/tmux-my/tmux-my.only.conf".source = ./etc/tmux-my/tmux-my.only.conf;
+      ".oh-my-zsh-custom/plugins/tmux-my/tmux-my.plugin.zsh".source = ./etc/tmux-my/tmux-my.plugin.zsh;
     };
 
     xsession.enable = true;
