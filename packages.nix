a2bbdd169bf86284ccd0a1b547f0a225a262ac20
Mikhail Klementev <blame@dumpstack.io>
2019-07-22 17:34:36 +0000

    Add "git get"

--- packages.nix
+++ packages.nix
@@ -161,5 +161,7 @@ in {
     unstable.firefox unstable.stellarium
 
     (writeShellScriptBin "torbrowser" "${unstable.tor-browser-unwrapped}/bin/firefox")
+
+    (writeShellScriptBin "git-get" "${git}/bin/git clone https://$1 $GOPATH/src/$1")
   ];
 }
