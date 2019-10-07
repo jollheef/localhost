f22d35c6313c65d348ee41d9114176f25fbcde4d
Mikhail Klementev <blame@dumpstack.io>
2019-10-07 21:04:56 +0000

    Add zig

--- packages.nix
+++ packages.nix
@@ -76,7 +76,7 @@ in {
       (with epkgs.melpaPackages; [
         # Programming languages modes
         haskell-mode rust-mode scala-mode csharp-mode d-mode
-        solidity-mode php-mode go-mode ponylang-mode
+        solidity-mode php-mode go-mode ponylang-mode zig-mode
         # Development
         helm-gtags slime xcscope go-autocomplete
         # Configuration languages modes
@@ -104,6 +104,7 @@ in {
     maven binutils-unwrapped openssl bison flex fop libxslt
     cmake manpages unstable.gradle cargo rustc guile hydra
     gitRepo rustfmt bazel ghc zlib gperf ccache opencv gotools
+    unstable.zig
 
     # re
     radare2 radare2-cutter
