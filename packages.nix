1dd8938e729da21a20aefdab5a6664c8a5cad5b9
Mikhail Klementev <blame@dumpstack.io>
2019-11-19 16:31:48 +0000

    Add tesseract

--- packages.nix
+++ packages.nix
@@ -57,7 +57,7 @@ in {
     pdftk vnstat dunst ghostscript graphicsmagick
     browsh texlive.combined.scheme-full rubber pandoc
     jq socat ffmpeg-full exiftool apktool mdl wine zstd
-    unstable.cointop
+    unstable.cointop unstable.tesseract
 
     # gpg-related
     gnupg unstable.yubikey-manager unstable.yubikey-personalization
