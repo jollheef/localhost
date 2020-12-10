dd58ef4b8f8915010b17ea49aa2e87317a50b3bf
Mikhail Klementev <blame@dumpstack.io>
2020-12-10 11:05:22 +0000

    Add rdesktop

--- packages.nix
+++ packages.nix
@@ -112,7 +112,7 @@ in {
     # x apps
     escrotum wire-desktop evince gimp gnome3.gnome-maps
     android-file-transfer libreoffice electrum gnome3.nautilus
-    signal-desktop
+    signal-desktop rdesktop
 
     (writeShellScriptBin "git-get" "${git}/bin/git clone https://$1 $GOPATH/src/$1")
 
