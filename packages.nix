eae48ae758355b3cebe3e2233599373a78a652cb
Mikhail Klementev <blame@dumpstack.io>
2022-05-11 14:32:28 +0000

    Remove imjtool

--- packages.nix
+++ packages.nix
@@ -143,34 +143,5 @@ in {
                                        --start-maximized \
                                        $@
     '')
-
-    (stdenv.mkDerivation {
-      pname = "imjtool";
-      version = "1.0.0";
-
-      dontConfigure = true;
-      dontBuild = true;
-      dontStrip = true;
-
-      src = fetchurl {
-        url = "http://newandroidbook.com/tools/imjtool.tgz";
-        sha256 = "sha256:027zlxsssfffhrlgfamcjn4whcarm8gh687xswz3mbmyra0rgspd";
-      };
-
-      setSourceRoot = "sourceRoot=`pwd`";
-
-      nativeBuildInputs = [ autoPatchelfHook ];
-      buildInputs = [
-        zlib lzma bzip2
-      ];
-      propagatedBuildInputs = [
-        lz4
-      ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        cp imjtool.ELF64 $out/bin/imjtool
-      '';
-    })
   ];
 }
